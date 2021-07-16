aa03848d1ac59bb69bb6178a1bd4ed8256921409
Sstark97 <aitorscinfo@gmail.com>
2021-07-16 14:17:01 +0100

    Add uuid

--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { Flex, Text } from "@chakra-ui/react";
+import { v4 as uuidv4 } from "uuid";
 import { connect } from "react-redux";
 import { getPlaces } from "../actions";
 import PlaceCard from "../components/PlaceCard";
