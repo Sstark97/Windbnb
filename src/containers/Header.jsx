223cabc7bb2f58120d5420e7b6e011a6fb030f8b
Sstark97 <aitorscinfo@gmail.com>
2021-07-16 13:18:41 +0100

    Header 1.0

--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,29 +1,54 @@
 import React from "react";
-import { Flex, Image, Select, Button } from "@chakra-ui/react";
+import { Flex, Image, Text, Button } from "@chakra-ui/react";
 import logo from "../assets/static/logo.png";
 import { AiOutlineSearch } from "react-icons/ai";
 
 const Header = () => {
   return (
     <Flex direction="column">
-      <Flex width="10rem"><Image src={logo} /></Flex>
+      <Flex width="10rem">
+        <Image src={logo} />
+      </Flex>
 
-      <Flex>
-        <Select placeholder="Select option">
-          <option value="option1">Option 1</option>
-          <option value="option2">Option 2</option>
-          <option value="option3">Option 3</option>
-        </Select>
+      <Flex
+        width="29.7rem"
+        height="5.5rem"
+        alignItems="center"
+        justifyContent="center"
+        alignSelf="center"
+        borderRadius="1rem"
+        border="1px solid #F2F2F2"
+        marginTop="3.9rem"
+        boxShadow="0px 1px 6px rgba(0, 0, 0, 0.1)"
+      >
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          width="13.8rem"
+          height="100%"
+          borderRight="1px solid #F2F2F2"
+        >
+          <Text fontSize="2xl"> Helsinki, Finland </Text>
+        </Flex>
 
-        <Select placeholder="Select option">
-          <option value="option1">Option 1</option>
-          <option value="option2">Option 2</option>
-          <option value="option3">Option 3</option>
-        </Select>
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          width="10.6rem"
+          height="100%"
+          borderRight="1px solid #F2F2F2"
+        >
+          <Text fontSize="2xl" color="#BDBDBD"> Add guests </Text>
+        </Flex>
 
-        <Button>
-          <AiOutlineSearch size="3em" />
-        </Button>
+        <Flex
+          width="5.3rem"
+          height="100%"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <AiOutlineSearch size="1.7rem" color="#EB5757E5"/>
+        </Flex>
       </Flex>
     </Flex>
   );
